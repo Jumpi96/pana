a9316910ac941bb3a4ef3c516dbaeb2dec63a197
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2025-12-23 12:36:51 -0300

    Remove rebalance calculations for macros
    Removed calculations for protein, carbs, and fat rebalance.

--- src/pages/WeeklyView.tsx
+++ src/pages/WeeklyView.tsx
@@ -108,18 +108,6 @@ export function WeeklyView() {
     ? calculateWeeklyRebalance(totalsCompleted.calories, expected.calories * 7, daysElapsed, daysRemaining)
     : 0
 
-  const rebalanceProtein = (totalsCompleted && expected && daysRemaining > 0)
-    ? calculateWeeklyRebalance(totalsCompleted.protein_g, expected.protein_g * 7, daysElapsed, daysRemaining, 1)
-    : 0
-
-  const rebalanceCarbs = (totalsCompleted && expected && daysRemaining > 0)
-    ? calculateWeeklyRebalance(totalsCompleted.carbs_g, expected.carbs_g * 7, daysElapsed, daysRemaining, 1)
-    : 0
-
-  const rebalanceFat = (totalsCompleted && expected && daysRemaining > 0)
-    ? calculateWeeklyRebalance(totalsCompleted.fat_g, expected.fat_g * 7, daysElapsed, daysRemaining, 1)
-    : 0
-
   const macroTips = macroOverages ? [
     { label: 'Protein', value: macroOverages.protein_g, emoji: '🥩', msg: 'Focus on lean meats or shakes next time!' },
     { label: 'Carbs', value: macroOverages.carbs_g, emoji: '🍞', msg: 'Maybe swap the bread for some greens?' },
